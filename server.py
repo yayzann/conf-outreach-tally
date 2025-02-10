2a403e5a51f28548db5da7a14b9497f5900ba69d
Yazan Ardekany <36682923+yayzann@users.noreply.github.com>
2025-02-09 22:37:45 -0800

    took it off port 5000 lol

--- server.py
+++ server.py
@@ -30,4 +30,4 @@ def get_conf_data():
     except Exception as e:
         return jsonify({"error":str(e)}),500
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    app.run(debug=True)
